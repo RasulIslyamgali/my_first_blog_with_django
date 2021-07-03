5ab3ef1a8afc50d87c583c9c79abccd249784c1d
rasul <rasul.islyamgali@gmail.com>
2021-07-03 22:13:19 +0300

    Add able to comment.

--- blog/urls.py
+++ blog/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('django.contrib.auth.urls')),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
